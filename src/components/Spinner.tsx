377baef04068fa52086b31ea0bad38697bb04bb6
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-02-18 11:32:28 +0200

    Feature: create custom spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,6 @@
+import { Image } from 'antd';
+import { FC } from 'react';
+
+export const Spinner: FC = () => (
+  <Image src="https://i.giphy.com/media/Wkcw6SzOtaSxG/giphy.webp" width={400} height={300} />
+);
