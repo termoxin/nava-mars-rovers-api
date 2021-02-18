124bb2924022fe5b967ca1ee99e3299751d5172d
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-02-18 11:36:43 +0200

    Feature: init NASA sdk and render pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
 import { FC } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App: FC = () => <h1>NASA API</h1>;
+import { RoverPage } from './pages/Rover';
+import { HomePage } from './pages/Home';
+
+import { NasaSDK } from './api/NasaSDK';
+import 'antd/dist/antd.css';
+
+const API_URL = process.env.REACT_APP_NASA_API_URL;
+const API_KEY = process.env.REACT_APP_NASA_API_KEY;
+
+if (API_URL && API_KEY) {
+  NasaSDK.getInstance({
+    url: API_URL,
+    apiKey: API_KEY,
+  });
+} else {
+  throw Error('Please, define env variables to make the app working');
+}
+
+const App: FC = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route path="/:name" exact>
+        <RoverPage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
