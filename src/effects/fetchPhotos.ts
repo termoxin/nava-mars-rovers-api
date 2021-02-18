e2e8ca37eea2b38fd42254ef1bf2ef0f752f5800
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-02-18 17:52:08 +0200

    Feature: add hasPhotos flag

--- src/effects/fetchPhotos.ts
+++ src/effects/fetchPhotos.ts
@@ -5,17 +5,28 @@ import RoversState from '../store/Rovers';
 const defaultParams = {
   sol: `${RoversState.defaultSol}`,
   page: '1',
+  camera: 'fhaz',
 };
+
+interface fetchPhotosReturn {
+  hasPhotos: boolean;
+}
 
 export const fetchPhotos = async (
   name: string,
+  sol: number,
+  filter: string,
   params: fetchRoverPhotosParams = defaultParams
-): Promise<void> => {
-  const { photos } = await NasaSDK.getInstance().fetchRoverPhotos(name, params);
-
+): Promise<fetchPhotosReturn> => {
   const rover = RoversState.getRoverByName(name);
 
+  const { photos } = await NasaSDK.getInstance().fetchRoverPhotos(name, params);
+
   if (rover) {
-    RoversState.setRoverPhotos(rover.id, photos);
+    RoversState.setRoverPhotos(rover.id, sol, filter, photos);
+
+    return { hasPhotos: !!photos.length };
   }
+
+  return { hasPhotos: true };
 };
